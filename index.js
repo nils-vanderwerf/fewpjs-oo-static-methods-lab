05535f9e066ae40ff1896fa17d02e3395eeafef8
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-27 21:48:34 +1100

    Passing all tests

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+  
+    let nonAlphaNum = ["!", "@", "#", "&", "(", ")", "[", "{", "}", "]", ":", ";", ",", "?", "/", "*", "`", "~", "$", "^", "+", "=",
+    "<", ">", '"'];
+    let newString = string.split("")
+    console.log(newString)
+    newString = newString.filter(l => !nonAlphaNum.includes(l))
+    return newString.join('')
+  }
+
+  
+
+  static titleize (string) {
+    
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    let result = []
+
+    let splitString = string.split(" ")
+
+    for (let n=0; n < splitString.length; n++) {
+      if (n==0) {
+        result.push( this.capitalize(splitString[n]) )
+      } else if (exceptions.includes(splitString[n])) {
+          result.push(splitString[n])
+      } else {
+          result.push( this.capitalize(splitString[n]) )
+      }
+    }
+    
+    return result.join(" ")
+  }
 }
+
+console.log(Formatter.titleize('in the night kitchen'))
+
+console.log(`No 44: ${Formatter.sanitize('ca$@#tching cold')}`)
